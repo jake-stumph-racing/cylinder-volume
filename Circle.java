0ebe74a1a6778fb4ac551c15d35f5b93a13d588c
Jake Stumph <72287324+jake-stumph-racing@users.noreply.github.com>
2021-10-28 13:27:01 -0700

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius){
+        if(radius < 0 ){
+            radius = 0;
+        }
+        this.radius = radius;
+    }
+
+    public double getRadius(){
+        return this.radius;
+    }
+
+    public double getArea(){
+        return (Math.PI * radius * radius);
+    }
+}
